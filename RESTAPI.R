809eed58672133771f49962ab91c3f2af42631f0
tylerjrichards <tylerjrichards@gmail.com>
2018-01-09 11:31:25 -0500

    added other state's databases

--- RESTAPI.R
+++ RESTAPI.R
@@ -3,11 +3,11 @@
 
 
 #* @get /geocode
-geocode <- function(location){
+geocode <- function(location, state){
   suppressMessages(library(dplyr))
+  
   Colorado <- tbl(US_db, "Colorado_data")
   Addresses = as.data.frame(select(Colorado, Address, Long, Lat, State))
-  state = "Colorado"
   long = Addresses$Long
   long
   result = c(long, state)
